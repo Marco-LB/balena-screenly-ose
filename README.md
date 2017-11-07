ae4e92b96b6067465b4b14beefb66a25dedb24c1
Gergely Imreh <imrehg@gmail.com>
2017-11-07 15:45:18 +0000

    enabling device public URL

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ Setting up [Screenly OSE](https://www.screenly.io/ose/) with [resin.io](https://
 
 ## Setup
 
-* Follow the [getting started guide](https://docs.resin.io/raspberrypi3/python/getting-started/) regarding how to sign up to resin.io, create an application, and add a device.
-* `git clone` this repository, then you can `git push` to your resin.io/Screenly application.
-* Once the device downloads the application image, you'll see the splash screen and can navigate to the control dashboard
-* In the resin.io dashboard, application or device, and `RESIN_HOST_CONFIG_gpu_mem` to a value at least `192` in the Fleet Configuration or Device Configurations menu (respectcively). If set in the Fleet Configuration, it will apply to all devices (present and future).
-* If you would like to see more debug information in the dashboard, in the Environment Variables section set `DEBUG` to `1` (if don't want debug info later, just remove that environment variable)
+*   Follow the [getting started guide](https://docs.resin.io/raspberrypi3/python/getting-started/) regarding how to sign up to resin.io, create an application, and add a device.
+*   `git clone` this repository, then you can `git push` to your resin.io/Screenly application.
+*   Once the device downloads the application image, you'll see the splash screen and can navigate to the control dashboard
+*   In the resin.io dashboard, application or device, and `RESIN_HOST_CONFIG_gpu_mem` to a value at least `192` in the Fleet Configuration or Device Configurations menu (respectcively). If set in the Fleet Configuration, it will apply to all devices (present and future).
+
+### Environment Variables
+
+Set them for example in the Environment Variables section in the resin.io dashboard:
+
+*   `DEBUG`: If you would like to see more debug information in the dashboard, set `1` (if don't want debug info later, just remove that environment variable)
+*   `MANAGEMENT_USER` and `MANAGEMENT_PASSWORD`: used to set the authentication user and passoword for the management interface. Setting them to empty values sets no user/password, removing them won't change the settings
+*   `OVERWRITE_CONFIG`: set to `1` to overwrite the existing `screenly.config` by the one provided in this repository, delete the variable if next time the config should not be overwritten
+*   `NO_PORT_FORDWARD`: set it to `1` not to set up port forwarding from port 80 -> 8080
--- startup.sh
+++ startup.sh
@@ -6,21 +6,30 @@ echo "Starting up Screenly..."
 umount /dev/shm && mount -t tmpfs shm /dev/shm
 
 # Create folders in /data if needed and copy default files
-if [ ! -d "$SCREENLY_HOME" ]; then
-    echo "SCREENLY_HOME at ${SCREENLY_HOME} does not exists, setting up"
+if [ ! -d "${SCREENLY_HOME}" ]; then
+    echo "SCREENLY_HOME at ${SCREENLY_HOME} does not exists, setting up."
     mkdir -p "${SCREENLY_HOME}"
     mkdir "${SCREENLY_HOME}/screenly"
     mkdir "${SCREENLY_HOME}/.screenly"
     mkdir "${SCREENLY_HOME}/screenly_assets"
     mkdir -p "${SCREENLY_HOME}/.config/uzbl"
-    cp -n screenly.conf "${SCREENLY_HOME}/.screenly/screenly.conf"
-    cp -n screenly/ansible/roles/screenly/files/screenly.db "${SCREENLY_HOME}/.screenly/screenly.db"
-    cp -n screenly/loading.png "${SCREENLY_HOME}/screenly/loading.png"
-    cp -n screenly/ansible/roles/screenly/files/uzbl-config "${SCREENLY_HOME}/.config/uzbl/config-screenly"
+    cp screenly/ansible/roles/screenly/files/screenly.db "${SCREENLY_HOME}/.screenly/screenly.db"
+    cp screenly/loading.png "${SCREENLY_HOME}/screenly/loading.png"
+    cp screenly/ansible/roles/screenly/files/uzbl-config "${SCREENLY_HOME}/.config/uzbl/config-screenly"
     # set the permissions right
     chown -R pi:pi "${SCREENLY_HOME}"
 else
     echo "SCREENLY_HOME at ${SCREENLY_HOME} already exists"
+    if [ -n "${OVERWRITE_CONFIG}" ]; then
+        echo "Requested to overwrite Screenly config file."
+        cp screenly.conf "${SCREENLY_HOME}/.screenly/screenly.conf"
+    fi
+fi
+
+# Set management page's user and password from environment variables,
+# but only if both of them are provided. Can have empty values provided.
+if [ -n "${MANAGEMENT_USER+x}" ] && [ -n "${MANAGEMENT_PASSWORD+x}" ]; then
+    sed -i -e "s/^user = .*/user = ${MANAGEMENT_USER}/" -e "s/^password = .*/password = ${MANAGEMENT_PASSWORD}/" "${SCREENLY_HOME}/.screenly/screenly.conf"
 fi
 
 # Start services after everything's set up
@@ -30,6 +39,28 @@ systemctl start screenly-viewer.service
 systemctl start screenly-web.service
 systemctl start screenly-websocket_server_layer.service
 
-if [ -n "$DEBUG" ]; then
+# If extra debug information is required in the console, then show the container's
+# journal in the logs by setting the `DEBUG` environment variable to anything.
+if [ -n "${DEBUG}" ]; then
     journalctl --follow --all --output=short
 fi
+
+# If device public URL is set up, port 80 has to be redirected to 8080 where the
+# management interface runs.
+# If that redirection is not required, then can set `NO_PORT_FORDWARD` environemt
+# variable to disable this.
+function remove_port_forwarding {
+    echo "Cleaning up port forwarding."
+    iptables -t nat -D PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+}
+if [ -z "${NO_PORT_FORDWARD}" ]; then
+    # Setting up redirect of port 80 to 8080, so the device can be managed remotely
+    iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+    # Clean u
+    trap 'remove_port_forwarding' SIGINT SIGTERM
+fi
+
+# Idle so not to exit
+while : ; do
+    sleep 600;
+done
